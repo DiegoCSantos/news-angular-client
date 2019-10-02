83112f4221ffcfae7f0148ecf4f5a1099543368b
Diego Correia Dos Santos <diego.santos@techne.com.br>
2019-10-02 10:47:08 -0300

    cria modulos e componentes de entidades

---
+++ news-client/src/app/admin/file/file.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FileComponent } from './file/file.component';
+
+
+
+@NgModule({
+  declarations: [FileComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class FileModule { }
